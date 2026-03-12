3935bd4c1ef9fe1a57b14cbdd130795cd379e264
Macarena Morales <matryoskka@gmail.com>
2026-03-12 12:58:34 +0100

    feat: modelos del blog y configuracion avanzada del admin completados

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from .models import Post, Comment
 
-# Register your models here.
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('title', 'author', 'is_published', 'created_at')
+    list_filter = ('is_published', 'created_at')
+    search_fields = ('title', 'content')
+    # ¡El toque Senior! Autocompleta el slug basado en el título
+    prepopulated_fields = {'slug': ('title',)}
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ('post', 'user', 'created_at')
+    search_fields = ('comment', 'user__email')
--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,35 @@
 from django.db import models
+from django.conf import settings # Para enlazar con tu CustomUser
 
-# Create your models here.
+class Post(models.Model):
+    # El autor del artículo
+    author = models.ForeignKey(
+        settings.AUTH_USER_MODEL, 
+        on_delete=models.CASCADE, 
+        related_name='blog_posts'
+    )
+    title = models.CharField(max_length=200)
+    # El 'slug' es la versión del título para la URL (ej: mi-primer-articulo)
+    slug = models.SlugField(max_length=200, unique=True)
+    content = models.TextField()
+    image_url = models.TextField(blank=True, null=True) 
+    is_published = models.BooleanField(default=False) # TINYINT(1) en tu diagrama
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
+
+class Comment(models.Model):
+    # A qué artículo pertenece el comentario
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='comments')
+    # Qué usuario lo ha escrito
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL, 
+        on_delete=models.CASCADE, 
+        related_name='user_comments'
+    )
+    comment = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"Comentario de {self.user.email} en {self.post.title}"
